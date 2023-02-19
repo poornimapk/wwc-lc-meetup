0cf053f77af3886dddf15177ddf15551b43e9619
poornimapk <poornima.pk@gmail.com>
2023-02-19 11:07:24 -0800

    TwoSum BruteForce

---
+++ src/main/scala/TwoSum.scala
@@ -0,0 +1,42 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+object TwoSum {
+  def twoSumBrute(nums: Array[Int], target: Int): Array[Int] = {
+    var myArr = Array[Int]()
+    var i = 0
+    var flag = false
+    while (i < nums.length - 1 && !flag) {
+      var j = i + 1
+      while(j < nums.length && !flag) {
+        if (nums(i) + nums(j) == target) {
+          myArr = myArr :+ i
+          myArr = myArr :+ j
+          flag = true
+        }
+        j += 1
+      }
+      i += 1
+    }
+    myArr
+  }
+
+//  def twoSumOptimized(nums: Array[Int], target: Int): Array[Int] = {
+//
+//  }
+}
--- src/test/scala/UnitTest.scala
+++ src/test/scala/UnitTest.scala
@@ -10,4 +10,10 @@ class UnitTest extends AnyFunSuite {
     assert(ContainsDuplicate.containsDuplicateFromLC(Array(1, 2, 3, 4)) === false)
     assert(ContainsDuplicate.containsDuplicateFromLC(Array(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)) === true)
   }
+
+  test("TwoSumBruteForce") {
+    assert(TwoSum.twoSumBrute(Array(2,7,11,15), 9) === Array(0, 1))
+    assert(TwoSum.twoSumBrute(Array(3, 2, 4), 6) === Array(1, 2))
+    assert(TwoSum.twoSumBrute(Array(3, 3), 6) === Array(0, 1))
+  }
 }
